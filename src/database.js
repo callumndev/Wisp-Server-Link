92413fc0f47b1e80f77e0eb1bfaa30e3cf1a0429
CN <callumnewsome100@gmail.com>
2022-03-13 12:02:24 +0000

    Update database

---
+++ src/database.js
@@ -0,0 +1,38 @@
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = class DataBase {
+    #sequelize;
+    #models = new Map();
+
+    constructor({ hostname, port, username, password, database }) {
+        this.#sequelize = new Sequelize(database, username, password, {
+            host: hostname,
+            port: port,
+            logging: false,
+            dialect: "mysql"
+        });
+
+        const modelsPath = path.join(__dirname, "models");
+        const models = fs.readdirSync(modelsPath).map(model => ({
+            name: model.replace(".js", ""),
+            model: require(path.join(modelsPath, model))
+        }));
+        for (const model of models) {
+            this.#models.set(model.name, this.#sequelize.define(model.name, model.model, {
+                sequelize: this.#sequelize,
+                tableName: model.name,
+                timestamps: false
+            }));
+            this.#models.get(model.name).sync();
+        }
+    }
+
+    get(model) {
+        if (!this.#models.has(model)) {
+            throw `Unknown model "${model}"`;
+        }
+        return this.#models.get(model);
+    }
+}
