df292ae69a8dee53fe2e095b805cef4017b3de2f
lzz <callumnewsome100@gmail.com>
2022-03-15 01:53:33 +0000

    Add models for db infamous_iga

--- src/models/infamous_iga/request.js
+++ src/models/infamous_iga/request.js
@@ -1,11 +1,5 @@
 const { DataTypes, Sequelize } = require("sequelize");
 
-// review_message: review.id,
-// review_channel: review.channel.id,
-// //
-// request_message: msg.id,
-// request_channel: msg.channel.id
-
 module.exports = {
     id: {
         type: Sequelize.INTEGER,
@@ -13,8 +7,5 @@ module.exports = {
         primaryKey: true
     },
     message: Sequelize.STRING,
-    channel: Sequelize.STRING,
-    requester: Sequelize.STRING,
     command: Sequelize.STRING,
-    server: Sequelize.STRING,
 }
